631808bfd9950fed844a7599a327999d2533d6c2
EduardoCortezB <eduardocortezzeus@gmail.com>
2023-02-06 01:05:45 -0600

    done: 1_data_types number_exercises

--- ruby_basics/1_data_types/exercises/numbers_exercises.rb
+++ ruby_basics/1_data_types/exercises/numbers_exercises.rb
@@ -25,10 +25,12 @@ end
 
 def float_division(a, b)
   # return the result of dividing a by b as a float, rather than an integer
+  (a/b).to_f
 end
 
 def string_to_number(string)
   # return the result of converting a string into an integer
+  string.to_i
 end
 
 def even?(number)
